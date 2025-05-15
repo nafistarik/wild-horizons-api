d87a479b9ad7e9a4f2f84de287190beb6e051d00
nafistarik <nafistarik789@gmail.com>
2025-05-15 18:56:17 +0600

    Continent based filtered api done

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-import http from 'node:http';
-import { getDataFromDB } from './database/db.js';
+import http from "node:http";
+import { getDataFromDB } from "./database/db.js";
 
 const PORT = 8000;
 
-const server = http.createServer( async (req, res) => {
-    const allDestinations = await getDataFromDB();
-    if (req.url === '/api' && req.method === 'GET') {
-        res.setHeader('Content-Type', 'application/json');
-        res.statusCode = 200;
-        res.end(JSON.stringify(allDestinations));
-    } else {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({
-            error: 'Not Found',
-            message: 'The requested route does not exist.',
-        }));
-    }
-})
+const server = http.createServer(async (req, res) => {
+  const allDestinations = await getDataFromDB();
+  if (req.url === "/api" && req.method === "GET") {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
+    res.end(JSON.stringify(allDestinations));
+  } else if (req.url.startsWith("/api/continent") && req.method === "GET") {
+    const continent = req.url.split("/").pop(); //req.url.split('/')[3]
+    const filteredDestinations = allDestinations.filter((destination) => {
+      return (
+        destination.continent.toLocaleLowerCase() ===
+        continent.toLocaleLowerCase()
+      );
+    });
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
+    res.end(JSON.stringify(filteredDestinations));
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "application/json");
+    res.end(
+      JSON.stringify({
+        error: "Not Found",
+        message: "The requested route does not exist.",
+      })
+    );
+  }
+});
 
-server.listen( PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-})
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
